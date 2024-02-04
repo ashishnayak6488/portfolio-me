9766f06b019961e939fc40eaa013d6db67277851
Ashish <ashishnayak6488@gmail.com>
2024-02-04 23:44:14 +0530

    deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const connection = require('./config/dbConfig')
 const dotenv = require('dotenv')
-const cors = require('cors')
+// const cors = require('cors')
 
 const portfolioRoute = require('./routers/portfolioRouter')
 
@@ -9,10 +9,21 @@ dotenv.config();
 
 const app  = express();
 app.use(express.json());
-app.use(cors())
+// app.use(cors())
 app.use('/api/portfolio' , portfolioRoute);
 
 const port = process.env.PORT || 5000;
+
+const path =  require('path');
+
+if (process.env.NODE_ENV === 'production') {
+    
+    app.use(express.static(path.join(__dirname , "client/build")));
+    app.get("*" , (req , res) =>{
+        res.sendFile(path.join(__dirname , "client/build/index.html"));
+    });
+}
+
 
 app.listen(port , console.log(`Server Running on Port ${port}`))
 

ef7253be9667a42aa23f8053f12e98b7ae2aa6a6
ASHISH KUMAR <114363439+ashishnayak6488@users.noreply.github.com>
2024-07-30 22:30:56 +0530

    add experience1

--- server.js
+++ server.js
@@ -1,35 +1,34 @@
-const express = require('express')
-const connection = require('./config/dbConfig')
-const dotenv = require('dotenv')
-// const cors = require('cors')
+const express = require("express");
+const connection = require("./config/dbConfig");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-const portfolioRoute = require('./routers/portfolioRouter')
+const portfolioRoute = require("./routers/portfolioRouter");
 
 dotenv.config();
 
-const app  = express();
+const app = express();
 app.use(express.json());
-// app.use(cors())
-app.use('/api/portfolio' , portfolioRoute);
+app.use(cors());
+app.use("/api/portfolio", portfolioRoute);
 
 const port = process.env.PORT || 5000;
 
-const path =  require('path');
+const path = require("path");
 
 // if (process.env.NODE_ENV === 'production') {
-    
+
 //     app.use(express.static(path.join(__dirname , "client/build")));
 //     app.get("*" , (req , res) =>{
 //         res.sendFile(path.join(__dirname , "client/build/index.html"));
 //     });
 // }
 
-    app.use(express.static(path.join(__dirname , "./client/build")));
-    app.get("*" , (req , res) =>{
-        res.sendFile(path.join(__dirname , "./client/build/index.html"));
-    });
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
-
-app.listen(port , console.log(`Server Running on Port ${port}`))
+app.listen(port, console.log(`Server Running on Port ${port}`));
 
 connection();

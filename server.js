2b68f912caebba64d7c242c823fb5ea606438ab2
ASHISH KUMAR <114363439+ashishnayak6488@users.noreply.github.com>
2024-08-01 11:06:23 +0530

    vercel.json

--- server.js
+++ server.js
@@ -1,7 +1,54 @@
+// const express = require("express");
+// const connection = require("./config/dbConfig");
+// const dotenv = require("dotenv");
+// const cors = require("cors");
+
+// const portfolioRoute = require("./routers/portfolioRouter");
+
+// dotenv.config();
+
+// const app = express();
+// app.use(express.json());
+// // app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: "https://portfolio-me-r8xa.vercel.app/",
+//     credentials: true,
+//   })
+// );
+// app.use("/api/portfolio", portfolioRoute);
+
+// const port = process.env.PORT || 5000;
+
+// const path = require("path");
+
+// // if (process.env.NODE_ENV === 'production') {
+
+// //     app.use(express.static(path.join(__dirname , "client/build")));
+// //     app.get("*" , (req , res) =>{
+// //         res.sendFile(path.join(__dirname , "client/build/index.html"));
+// //     });
+// // }
+
+// // app.use(express.static(path.join(__dirname, "./client/build")));
+// // app.get("*", (req, res) => {
+// //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// // });
+
+// app.get("/", (req, res) => {
+//   res.send("Welcome to my portfolio");
+// });
+
+// app.listen(port, console.log(`Server Running on Port ${port}`));
+
+// connection();
+
 const express = require("express");
 const connection = require("./config/dbConfig");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const serverless = require("serverless-http");
 
 const portfolioRoute = require("./routers/portfolioRouter");
 
@@ -9,7 +56,6 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
 
 app.use(
   cors({
@@ -19,27 +65,21 @@ app.use(
 );
 app.use("/api/portfolio", portfolioRoute);
 
-const port = process.env.PORT || 5000;
-
 const path = require("path");
 
-// if (process.env.NODE_ENV === 'production') {
-
-//     app.use(express.static(path.join(__dirname , "client/build")));
-//     app.get("*" , (req , res) =>{
-//         res.sendFile(path.join(__dirname , "client/build/index.html"));
-//     });
-// }
-
-// app.use(express.static(path.join(__dirname, "./client/build")));
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client/build/index.html"));
+  });
+}
 
 app.get("/", (req, res) => {
   res.send("Welcome to my portfolio");
 });
 
-app.listen(port, console.log(`Server Running on Port ${port}`));
-
+// Establish database connection
 connection();
+
+// Export the handler for serverless environment
+module.exports.handler = serverless(app);
